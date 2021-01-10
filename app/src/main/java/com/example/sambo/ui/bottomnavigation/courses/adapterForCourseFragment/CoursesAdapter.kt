0dc707cef9be68e143687ea121fdc68fb453f5e2
zhanysch <zhanysch@gmail.com>
2021-01-10 18:04:47 +0600

    осталось clicks

--- app/src/main/java/com/example/sambo/ui/bottomnavigation/courses/adapterForCourseFragment/CoursesAdapter.kt
+++ app/src/main/java/com/example/sambo/ui/bottomnavigation/courses/adapterForCourseFragment/CoursesAdapter.kt
@@ -1,18 +1,15 @@
 package com.example.sambo.ui.bottomnavigation.courses.adapterForCourseFragment
 
-import android.util.Log
-import com.example.sambo.data.modelcourses.Rows
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sambo.R
-import com.google.android.material.shape.CornerFamily
-import com.squareup.picasso.Callback
+import com.example.sambo.data.modelcourses.Rows
+import com.example.sambo.utils.setCornerRadius
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recycler_helper.view.*
-import java.lang.Exception
 
 class CoursesAdapter: PagedListAdapter<Rows,CoursesViewHolder>(DiffUtils.diffUtilDAta) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoursesViewHolder {
@@ -32,12 +29,12 @@ class CoursesViewHolder(view: View):RecyclerView.ViewHolder(view){
         Picasso.get().load(item?.preview).into(itemView.imageCourse)
 
         val radius = itemView.context.resources.getDimension(R.dimen.imageCutted)
-        val shape = itemView.imageCourse.shapeAppearanceModel.toBuilder()
-            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
-            .setTopRightCorner(CornerFamily.ROUNDED, radius)
-            .setBottomLeftCorner(CornerFamily.ROUNDED, radius)
-            .setBottomRightCorner(CornerFamily.ROUNDED, radius)
-            .build()
-        itemView.imageCourse.shapeAppearanceModel = shape
+
+        itemView.imageCourse.setCornerRadius(  // view Extension
+            topLeft = radius,
+            topRight = radius,
+            bottomLeft = radius,
+            bottomRight = radius
+        )
     }
 }
